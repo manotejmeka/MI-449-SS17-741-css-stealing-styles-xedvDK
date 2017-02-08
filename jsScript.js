9468d8db86c1f06d84b533007bca811e6c951655
Manotej Meka <manotejmeka@gmail.com>
2017-02-08 14:53:34 -0500

    Font fixed, copeied your code

--- jsScript.js
+++ jsScript.js
@@ -26,3 +26,28 @@ function hoverOn (x, i) {
 }
 
 title()
+
+/* Better way to code it
+function title () {
+  var titleTag = document.getElementsByTagName('H1')[0]
+  console.log(titleTag)
+  var titleTagHTML = titleTag.innerHTML.trim()
+  var i = 0
+  var titleSpan = ''
+  var style = document.createElement('style');
+  for (i = 0; i < titleTagHTML.length; i++) {
+    // console.log(titleTagHTML[i])
+    titleSpan += '<span class="title title-' + i + '">' + titleTagHTML[i] + '</span>'
+    css = '.title-' + i + ':hover { color: ' + CSS_COLOR_NAMES[i] + '; } '
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+  }
+  titleTag.innerHTML = titleSpan
+  document.getElementsByTagName('head')[0].appendChild(style);
+}
+
+title()
+*/
